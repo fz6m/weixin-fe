5927c0308546ec056e7a3ccdd76222ee8b664a9c
fz6m <59400654+fz6m@users.noreply.github.com>
2022-03-13 16:31:10 +0800

    feat: init project 🍓

---
+++ scripts/transform.ts
@@ -0,0 +1,60 @@
+
+import "zx/globals";
+import { cloneDeep, trim } from "lodash";
+
+interface IChunk {
+  start: number;
+  end: number;
+}
+
+const EMPTY_CHUNK: IChunk = { start: 0, end: 0 };
+
+const run = async () => {
+  const tplPath = path.join(__dirname, "../README.tpl.md");
+
+  const content = `${fs.readFileSync(tplPath, "utf-8")}\n`;
+
+  const lines = content.split("\n");
+  const chunks: IChunk[] = [];
+
+  let chunk: IChunk = cloneDeep(EMPTY_CHUNK);
+  lines.forEach((line, index) => {
+    // find end
+    if (chunk.start > 0 && !trim(line)?.length) {
+      chunk.end = index;
+      // save
+      chunks.push(chunk);
+      // reset chunk
+      chunk = cloneDeep(EMPTY_CHUNK);
+      return;
+    }
+    // find start
+    if (line.includes(`:-:|:-:`)) {
+      chunk.start = index + 1;
+      return;
+    }
+  });
+
+  const newLines = cloneDeep(lines);
+
+  const partTransform = (part: string[]) => {
+    // only replace once
+    part = part.map(l => l.replace('前端', '**前端**'))
+    part.sort((a, b) => a.localeCompare(b))
+    return part
+  }
+  chunks.forEach(({ start, end }) => {
+    const part = newLines.slice(start, end).map((l) => trim(l))
+    const sortedPart = [
+      ...partTransform(part.filter(l => l.includes('✓'))),
+      ...partTransform(part.filter(l => !l.includes('✓')))
+    ]
+    newLines.splice(start, end - start, ...sortedPart)
+  });
+
+  const newContent = `${newLines.join('\n')}\n`
+  const outputPath = path.join(__dirname, '../README.md')
+  fs.writeFileSync(outputPath, newContent, 'utf-8')
+};
+
+run();
